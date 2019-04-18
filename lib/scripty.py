58fe9bd4a8cbbaf22ab6eec51575cafc65b74386
joel-reujoe <joel.reujoe@gmail.com>
2019-04-18 21:37:34 +0530

    first commit

---
+++ lib/scripty.py
@@ -0,0 +1,63 @@
+import pickle
+import random
+
+import numpy as np
+from googletrans import Translator
+from nltk.collections import Counter
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import f1_score
+from sklearn.metrics.classification import accuracy_score
+from sklearn.tree import DecisionTreeClassifier as DTC
+import sys
+import warnings
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=UserWarning)
+stop_words = set(stopwords.words("english"))
+
+
+def create_lexicon(pos, neg):
+    lexicon = []
+    for file_name in [pos, neg]:
+        with open(file_name, 'r') as f:
+            contents = f.read()
+            for line in contents.split('\n'):
+                data = line.strip('\n')
+                if data:
+                    all_words = word_tokenize(data)
+                    lexicon += list(map((lambda x: x.lower()), all_words))
+    lexicons = []
+    for word in lexicon:
+        if not word in stop_words:
+            lexicons.append(word)
+    word_counts = Counter(lexicons)  # it will return kind of dictionary
+    l2 = []
+    for word in word_counts:
+        if 4000 > word_counts[word]:
+            l2.append(word)
+    return l2
+
+def check_class(data):
+    with open("decisiontree.pkl", "rb") as f:
+        clf = pickle.load(f)
+    prediction = clf.predict(data)
+    return np.array(prediction)
+
+line=sys.argv[1]
+translator=Translator()
+line=translator.translate(line,dest='en').text
+line = word_tokenize(line)
+words = list(set([w.lower() for w in line]))
+lexicons = create_lexicon('pos_english.txt', 'neg_english.txt')
+featureset = np.zeros(len(lexicons))
+for w in lexicons:
+  if w in words:
+    idx = lexicons.index(w.lower())
+    featureset[idx] += 1
+featureset = list(featureset)
+featureset = np.array(featureset)
+y_pred = list(check_class([featureset[:-17]]))
+print(y_pred)
+sys.stdout.flush()
---
+++ src/controller.ts
@@ -0,0 +1,33 @@
+var express=require('express')
+var admin=require('firebase-admin')
+var url = require('url');
+var path = require('path');
+var sql = require('mysql');
+var exec = require("child_process").exec;
+var bodyParser = require('body-parser')
+const app=express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/getResult',(req,res,next)=>{
+    console.log(req.query.text)
+    var dataString=""
+    var child=exec(`python scripty.py ${req.query.text}`)
+    child.stdout.on('data', function(data){
+        console.log(data)
+        dataString += data.toString();
+      });
+      child.stdout.on('end', function(){
+        console.log('result=',dataString);
+        res.send(dataString)
+      })
+      child.stdin.end();
+})
+app.listen(9000,()=>{
+    console.log("Server Active At 8000");  
+})
